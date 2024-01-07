e28046a8ebbc86b46dc6be172e2af2fcf8b2df5b
mooojn <144435962+mooojn@users.noreply.github.com>
2024-01-07 14:01:33 +0500

    if structure with logic gates

--- loops_and_if.rs
+++ loops_and_if.rs
@@ -1,6 +1,7 @@
 fn main(){
-    // just like range fn of python
+    // for loop
     println!("loop with step");
+    // just like range fn of python
     for i in (0..10).step_by(2){    
         println!("Hi count {}", i);
     }
@@ -8,10 +9,22 @@ fn main(){
     for i in 0..5{    
         println!("Hi count {}", i);
     }
-    println!("If condition");
-    let marks = 50;
+    
+    println!("If conditions");
+    let marks = 51;
     let age = 18;
-    if marks >= 50 && age == 18{
+    
+    // if condition's
+    if age < 18 || age > 18{
+        println!("You did'nt meet the age requirements");
+    }
+    else if marks < 50{
+        println!("Your score was below the expectations");
+    }
+    else if marks >= 50 && age == 18{
         println!("Eligible");
+    }
+    else{
+        println!("unexpected input");
     }
 }

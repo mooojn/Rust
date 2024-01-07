b434c36c9560b0ef1db86b5ec6ac8b579aea2fe7
mooojn <144435962+mooojn@users.noreply.github.com>
2024-01-07 13:11:58 +0500

    blocks during assignment

--- variables.rs
+++ variables.rs
@@ -1,10 +1,19 @@
 fn main()
 {
     let num: i32 = 8;
-    let character;
-    character = 'm';         // compiler infers the type at first initialization
-    // character = 's';      // immutable can't change
-
+    let character;           
+    character = 'm'; // compiler infers the type at first initialization
+    // character = 's'; // immutable can't change
+    let result = { // we can assign a result of a block during assignment 
+        if num > 8{
+            true
+        }
+        else{
+            false
+        }
+    };
+        
     println!("{}", num);
     println!("{}", character);
+    println!("{}", result);
 }
